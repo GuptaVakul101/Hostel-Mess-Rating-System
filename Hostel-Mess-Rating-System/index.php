0b060f9ecb7c179bb2682783814ee54de255b0f7
utkarshjain1508 <utkarshjain1508@gmail.com>
2019-04-27 17:38:14 +0530

    admin login created

--- Hostel-Mess-Rating-System/index.php
+++ Hostel-Mess-Rating-System/index.php
@@ -21,7 +21,7 @@ $q31 = mysqli_query($con,$q3);
 $introdisp = mysqli_fetch_array($q31);
 //echo $introdisp['colgintro'];
 
-//change footer 
+//change footer
 $q4 = "SELECT footerinfo FROM admin WHERE id=1";
 $q41 = mysqli_query($con,$q4);
 $footerdisp = mysqli_fetch_array($q41);
@@ -37,9 +37,9 @@ $footerdisp = mysqli_fetch_array($q41);
 	<script type="text/javascript" src="engine/js/visualslideshow.js"></script>
     <link rel="stylesheet" type="text/css" href="engine1/style.css" />
 	<script type="text/javascript" src="engine1/jquery.js"></script>
-    
+
     <style type="text/css" media="screen">
-#horizontalmenu ul 
+#horizontalmenu ul
 {
 padding:1; margin:1; list-style:none;
 }
@@ -49,7 +49,7 @@ float:left;
  position:relative;
 padding-right:50;
  display:block;
-border:0px solid #CC55FF; 
+border:0px solid #CC55FF;
 border-style:inset;
 }
 #horizontalmenu li ul
@@ -60,13 +60,13 @@ position:absolute;
 #horizontalmenu li:hover ul{
     display:block;
     background:#C4C4C4;
-height:auto; width:8em; 
+height:auto; width:8em;
 }
 #horizontalmenu li ul li
 {
     clear:both;
 border-style:none;}
-</style>	
+</style>
 </head>
 <table width="1050px" align="center"  border="1">
   <tbody>
@@ -96,7 +96,7 @@ border-style:none;}
 	<script type="text/javascript" src="engine1/wowslider.js"></script>
 	<script type="text/javascript" src="engine1/script.js"></script>
     <!--slider end-->
-    
+
       </th>
     </tr>
     <tr>
@@ -105,22 +105,24 @@ border-style:none;}
         <ul>
 		<li><a href="index.php" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;" ><b>HOME</b></a></li>
         <li><a href="index.php?option=about" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>ABOUT</b></a></li>
-		<li><a href="index.php?option=contact" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>CONTACT</b></a></li>	
+		<li><a href="index.php?option=contact" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>CONTACT</b></a></li>
         <li><a href="index.php?option=gallery" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>GALLERY</b></a></li>
         <li><a href="index.php?option=course" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>COURSES</b></a></li>
 		<li><a href="index.php?option=regs" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>REGISTRATION</b></a></li>
 		<li><a href="index.php?option=login" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>LOGIN</b></a>
+				<li><a href="admin" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;"><b>ADMIN LOGIN</b></a>
+
         	<!--<ul style="width:100%">
 				 <li style="width:100%"><a href="index.php?option=login" onMouseOver="this.style.color='#FFFFFF'" onMouseOut="this.style.color='#353535'" style="color:#353535 ; text-decoration:none;">Hostel Login</a></li>
 			 </ul>-->
               </li>
 		 </ul>
-                 
+
             </li>
-            
+
         </ul>
 </div>
-        
+
       </td>
     </tr>
     <tr>
@@ -184,12 +186,12 @@ border-style:none;}
 		case 'notice':
 		include('notice.php');
 		break;
-		
+
 	}}
 
 	?>
-	
-      
+
+
       </td>
       <td width="343" style="background-color:#468EFF">
       <center><font size="+2"><b style="color:#191B7E"><div style="background-color:#969BFB">College Updates</div><br></b></font></center>
