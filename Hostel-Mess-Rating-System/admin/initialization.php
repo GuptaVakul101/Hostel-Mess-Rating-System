87f6ce0fa6d2ed25197a72a44251b13a4bf45081
utkarshjain1508 <utkarshjain1508@gmail.com>
2019-05-01 04:57:37 +0530

    calculate ratings

--- Hostel-Mess-Rating-System/admin/initialization.php
+++ Hostel-Mess-Rating-System/admin/initialization.php
@@ -16,45 +16,37 @@ $query_result=mysqli_query($con,"SELECT * FROM mess");
 if (mysqli_num_rows($query_result) > 0) {
   while($row = mysqli_fetch_assoc($query_result)) {
     $messName=$row['messName'];
+    $total_count=0;
+    $score=0.0;
     $query=mysqli_query($con,"SELECT * FROM users WHERE Hostel_Mess='$messName'");
     if (mysqli_num_rows($query) > 0) {
       while($row2 = mysqli_fetch_assoc($query)) {
-
-        $feedback=$row2[]
+        $count=0;
+        $user_score=0;
+        $feedback=$row2[$cur_month];
+        $feedback_array=explode(" ",$feedback);
+        for ($x = 0; $x < sizeof($feedback_array); $x++){
+          if(array_key_exists($feedback_array[$x],$ratings)){
+            $user_score +=$ratings[$feedback_array[$x]];
+            $count++;
+          }
+        }
+        if($count){
+          $score +=(float)$user_score/$count;
+          $total_count++;
+        }
       }
     }
-
+    $final_rating=$score/$total_count;
+    mysqli_query($con,"UPDATE mess SET $cur_month=$final_rating WHERE messName='$messName'");
+    if($final_rating <5){
+      
+    }
   }
 }
 
 ?>
-<?php
-$timestamp = time();
-$q1 = "SELECT * FROM users";
-$result = mysqli_query($con,$q1);
-$str="";
-if (mysqli_num_rows($result) > 0) {
-  while($row = mysqli_fetch_assoc($result)) {
-      if($row["Mess_Request"] == ""){
-      }else{
-          $current_mess = $row["Hostel_Mess"];
-          $new_mess = $row["Mess_Request"];
-          $q2 = "SELECT managerUsername FROM mess WHERE messName = '$current_mess'";
-          $result1 = mysqli_query($con,$q2);
-          $q3 = "SELECT managerUsername FROM mess WHERE messName = '$new_mess'";
-          $result2 = mysqli_query($con,$q3);
-          $title1 = "";
-          $description1 = "";
-          $title2 = "";
-          $description2 = "";
-          $query = "INSERT INTO Notifications VALUES ('','$title1','$description1','$result1','$timestamp','Low')";
-          mysqli_query($con,$query);
-          $query = "INSERT INTO Notifications VALUES ('','$title2','$description2','$result2','$timestamp','Low')";
-          mysqli_query($con,$query);
-      }
-  }
-}
-?>
+
 
 
 
